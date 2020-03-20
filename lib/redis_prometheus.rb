442f9586c6e526a42339d7fa64ccb4c3fde44471
Andy Jeffries <andy@andyjeffries.co.uk>
2020-03-20 13:21:11 +0000

    Actually load the collector

--- lib/redis_prometheus.rb
+++ lib/redis_prometheus.rb
@@ -1,6 +1,7 @@
 require "redis_prometheus/middleware"
 require "redis_prometheus/railtie" if defined?(Rails)
 require "redis_prometheus/version"
+require "redis_prometheus/collector"
 
 module RedisPrometheus
   # Your code goes here...
